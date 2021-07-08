19dda9508bc2448e2f1e3931ab118cf3caf74a48
Julio Junior <juniorknx@hotmail.com>
2021-07-08 00:46:42 -0300

    add navmenu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,35 @@
 import '../Header/style.scss';
 import imglogo from '../../assets/imglogo.png'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faVirus } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
     return (
         <header>
             <div className="menu">
+                <div>
                     <h3>CORONAVÍRUS</h3>
                     <img src={imglogo} />
                     <span>BRASIL</span>
+                </div>
+                <nav>
+                <Link to="/">
+                    Início
+                </Link>
+
+                <Link to="/local">
+                    Sintomas
+                </Link>
+
+                <Link to="/">
+                    Dicas
+                </Link>
+
+                <Link to="/">
+                    Author
+                </Link>
+                </nav>
             </div>
         </header>
     )
