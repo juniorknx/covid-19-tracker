6068f63b5f43b7ba99ad48af8e47a27da336d5b0
Julio Junior <juniorknx@hotmail.com>
2021-07-29 00:19:48 -0300

    Ajuste na ordem dos itens no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,24 +8,25 @@ export function Footer(){
         <footer>
             <div className="footer-wrapper">
                 <div>
-                    <span>Desenvolvido por: Julio</span>
-                    <div className="social-icons">
-                        <a href="https://github.com/juniorknx" target="_blank" rel="Github">
-                            <img src={monstgithub} alt="Github" title="github" to="https://github.com/juniorknx" />
-                        </a>
+                <div className="social-icons">
+                    <a href="https://github.com/juniorknx" target="_blank" rel="Github">
+                        <img src={monstgithub} alt="Github" title="github" to="https://github.com/juniorknx" />
+                    </a>
 
-                        <a href="https://www.linkedin.com/in/julio-junior-ba665015b/" target="_blank" rel="Linkedin">
-                            <img src={linkedin} alt="linkedin" title="linkedin" />
-                        </a>
-                    </div>
+                    <a href="https://www.linkedin.com/in/julio-junior-ba665015b/" target="_blank" rel="Linkedin">
+                        <img src={linkedin} alt="linkedin" title="linkedin" />
+                    </a>
                 </div>
+                    <span>Desenvolvido por: Julio</span>
+                </div>
+
                 <div>
+                <div className="font-dados">
+                    <a href="https://servicos-datasus.saude.gov.br/" target="_blank" rel="Datasus">
+                        <img src={logodatasus} title="DataSus" alt="DataSus" />
+                    </a>
+                </div>
                     <span>Fonte de dados: API DataSus</span>
-                    <div className="font-dados">
-                        <a href="https://servicos-datasus.saude.gov.br/" target="_blank" rel="Datasus">
-                            <img src={logodatasus} title="DataSus" alt="DataSus" />
-                        </a>
-                    </div>
                 </div>
             </div>
         </footer>
